96f9f050afa736a73d84468cdb393e8338afe27a
DanielxDante <danielonline170600@outlook.com>
2023-07-18 16:52:10 +0800

    nextjs init

---
+++ Beautified Version/nextjs/src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <div>© 2023 Daniel Tay Jin Hong. All rights reserved </div>
+        <div></div>
+    </div>
+  )
+}
